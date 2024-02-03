ed373d3e6f3acc9a8c50424bfb3c583cffb5f1be
Christian Jay Mandani <mjhayxinadnam@gmail.com>
2024-02-03 23:14:00 +0800

    first commit

---
+++ DATE/Christian Jay Mandani - Activiy #5 - Switch Month.cpp
@@ -0,0 +1,60 @@
+// CHRISTIAN JAY MANDANI BSIT 1-1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //DECLARING VARIABLE
+    int date = 0;
+
+    //INPUT
+    cout << "Enter a number 1 - 12: ";
+    cin >> date;
+
+    // SWITCH STATEMENT
+    switch(date)
+    {
+        case 1:
+            cout << "Month of January!";
+            break;
+        case 2:
+            cout << "Month of February!";
+            break;
+        case 3:
+            cout << "Month of March!";
+            break;
+        case 4:
+            cout << "Month of April!";
+            break;
+        case 5:
+            cout << "Month of May!";
+            break;
+        case 6:
+            cout << "Month of June!";
+            break;
+        case 7:
+            cout << "Month of July!";
+            break;
+        case 8:
+            cout << "Month of August!";
+            break;
+        case 9:
+            cout << "Month of September!";
+            break;
+        case 10:
+            cout << "Month of October!";
+            break;
+        case 11:
+            cout << "Month of November!";
+            break;
+        case 12:
+            cout << "Month of December!";
+            break;
+
+    }
+        return 0;
+}
+
+
+  // BAWAL COPY PASTE!!!
